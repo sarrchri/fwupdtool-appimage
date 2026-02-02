628235524c1498bf3c1fee2273a1470337116410
Christian Sarre <102296983+sarrchri@users.noreply.github.com>
2026-02-02 13:11:42 +0200

    Install fwupd

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,11 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm fwupd
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+#get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
