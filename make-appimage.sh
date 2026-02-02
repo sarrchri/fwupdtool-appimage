099898e5e0f1b981812abfebb64683e43abb518a
Christian Sarre <christian@sarre.eu>
2026-02-02 13:23:15 +0200

    fwupd..

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,12 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON="/usr/share/icons/hicolor/scalable/apps/org.freedesktop.fwupd.svg"
+export DESKTOP=DUMMY
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/fwupdtool
 
 # Additional changes can be done in between here
 
