088ced47c10001a58bd44cdbaeee49ec001e9313
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-02 04:51:17 -0400

    set `MAIN_BIN`

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,6 +9,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON="/usr/share/icons/hicolor/scalable/apps/org.freedesktop.fwupd.svg"
+export MAIN_BIN=fwupdtool
 export DESKTOP=DUMMY
 
 # Deploy dependencies
