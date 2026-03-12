73c65cf3d9173fa703aa68ff0a3820f5db00778c
Christian Sarre <christian@sarre.eu>
2026-03-12 15:23:59 +0200

    Disable AppImage autoupdate-hook.

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,6 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q fwupd | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON="/usr/share/icons/hicolor/scalable/apps/org.freedesktop.fwupd.svg"
 export MAIN_BIN=fwupdtool
